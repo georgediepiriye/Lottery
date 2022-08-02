d00e2c584e9d522899460784ac438ae4288ba5ca
georgediepiriye <georgediepiriye4u@gmail.com>
2022-08-02 14:50:54 +0100

    added deployment scripts

---
+++ lottery-smart-contract/deploy/01-deploy-lottery.js
@@ -0,0 +1,57 @@
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  let vrfCoordinatorV2Address, subscriptionId;
+  const chainId = network.config.chainId;
+  const VRF_SUB_FUND_AMT = ethers.utils.parseEther("3");
+
+  if (developmentChains.includes(network.name)) {
+    // create VRFV2 Subscription
+    const vrfCoordinatorV2Mock = await ethers.getContract(
+      "VRFCoordinatorV2Mock"
+    );
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+    const transactionReceipt = await transactionResponse.wait();
+    subscriptionId = transactionReceipt.events[0].args.subId;
+    //fund the subscription
+    await vrfCoordinatorV2Mock.fundSubscription(
+      subscriptionId,
+      VRF_SUB_FUND_AMT
+    );
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+    subscriptionId = networkConfig[chainId]["subscriptionId"];
+  }
+
+  const entranceFee = networkConfig[chainId]["entranceFee"];
+  const gasLane = networkConfig[chainId]["gasLane"];
+  const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
+  const interval = networkConfig[chainId]["interval"];
+  const args = [
+    vrfCoordinatorV2Address,
+    entranceFee,
+    gasLane,
+    subscriptionId,
+    callbackGasLimit,
+    interval,
+  ];
+
+  const lottery = await deploy("Lottery", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+
+  log(`Our lottery address is ${lottery.address}`);
+  log("----------------------------------");
+};
+
+module.exports.tags = ["all", "lottery"];
--- lottery-smart-contract/hardhat.config.js
+++ lottery-smart-contract/hardhat.config.js
@@ -6,7 +6,32 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    rinkeby: {
+      chainId: 4,
+      blockConfirmations: 6,
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
   solidity: "0.8.7",
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
 };
